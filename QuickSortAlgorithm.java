9fadf9d7547dfa87ed3a0f02bbeb579d956e3f96
Gautam singh <97932898+rana603@users.noreply.github.com>
2022-08-24 03:40:19 +0530

    Update QuickSortAlgorithm.java

--- QuickSortAlgorithm.java
+++ QuickSortAlgorithm.java
@@ -34,3 +34,47 @@ public class QuickSortAlgorithm {
 
     }
 }
+
+Recusrsive method
+
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for (int i=0;i<n;i++)    arr[i]=sc.nextInt();
+         
+        recursiveQuickSort(arr,0,n-1);
+        for (int i=0;i<n;i++){
+            System.out.print(arr[i]+" ");
+        }System.out.println();
+        
+    }
+    public static void recursiveQuickSort(int[] array, int startIdx, int endIdx) { 
+        int idx = partition(array, startIdx, endIdx); 
+        if (startIdx < idx - 1) {
+            recursiveQuickSort(array, startIdx, idx - 1); } 
+        if (endIdx > idx) { 
+            recursiveQuickSort(array, idx, endIdx); } 
+    }
+    public static int partition(int[] array, int left, int right) { 
+        int pivot = array[left]; 
+        while (left <= right) { 
+            while (array[left] < pivot) { 
+                left++; }   
+            while (array[right] > pivot) { 
+                right--; } 
+            if (left <= right) { 
+                int tmp = array[left]; 
+                array[left] = array[right]; 
+                array[right] = tmp; 
+                left++; 
+                right--; 
+                
+            }
+        }
+        return left;
+    }
+}
+
