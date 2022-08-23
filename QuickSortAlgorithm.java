20e1eb92a77fbe01eb425896805ba13748c4bf9d
Gautam singh <97932898+rana603@users.noreply.github.com>
2022-08-24 03:50:46 +0530

    Update QuickSortAlgorithm.java

--- QuickSortAlgorithm.java
+++ QuickSortAlgorithm.java
@@ -52,11 +52,12 @@ class Main{
         
     }
     public static void recursiveQuickSort(int[] array, int startIdx, int endIdx) { 
-        int idx = partition(array, startIdx, endIdx); 
-        if (startIdx < idx - 1) {
-            recursiveQuickSort(array, startIdx, idx - 1); } 
-        if (endIdx > idx) { 
-            recursiveQuickSort(array, idx, endIdx); } 
+        if (startIdx<endIdx){
+            int idx = partition(array, startIdx, endIdx); 
+            recursiveQuickSort(array, startIdx, idx - 1); 
+            recursiveQuickSort(array, idx, endIdx); 
+            
+        } 
     }
     public static int partition(int[] array, int left, int right) { 
         int pivot = array[left]; 
