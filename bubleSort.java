43a30b7abed358cd8b1bd6b0a5b970882b7c7e9b
Gautam singh <97932898+rana603@users.noreply.github.com>
2022-08-24 03:53:09 +0530

    Update bubleSort.java

--- bubleSort.java
+++ bubleSort.java
@@ -16,3 +16,39 @@ public class bubleSort {
         System.out.println();
     }
 }
+
+Recusrsive BubleSort
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for (int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        bublesort(arr,n);
+        for (int i=0;i<n;i++){
+            
+            System.out.print(arr[i]+" ");
+        }
+        
+    }
+    public static void bublesort(int[] arr, int n){
+        if (n==1){
+            return;
+        }
+        for (int i=0;i<n-1;i++){
+            if (arr[i]>arr[i+1]){
+                swap(arr,i,i+1);
+            }
+        }
+        bublesort(arr,n-1);
+        
+    }
+    public static void swap(int[] arr,int i, int j){
+        int temp= arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+}
